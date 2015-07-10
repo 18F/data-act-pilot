71ac143cdd2b6eecdbdecb1d3faf1d73c27e58f6
Marco Segreto <msegreto@miceover.com>
2015-07-10 14:57:49 -0700

    Remove SAM key

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,7 +16,7 @@ sudo su vagrant <<'EOF'
 mkdir ~/.virtualenvs 2> /dev/null
 echo "export WORKON_HOME=~/.virtualenvs" >> ~/.bashrc
 echo "export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/bin/virtualenv" >> ~/.bashrc
-echo "export DOT_GOV_API_KEY=JhAodrs4c50i41FtKNCUxgfSUjeQ1jH1Pn7qIjLp" >> ~/.bashrc
+echo "export DOT_GOV_API_KEY='YOUR_SAM_API_KEY'" >> ~/.bashrc
 echo "export PYTHONPATH=/vagrant" >> ~/.bashrc
 echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
 source ~/.bashrc
