408011f41600d79b96b5fbf0f60b224557306b3f
msecret <msegreto@miceover.com>
2015-07-14 13:14:23 -0700

    Basic csv file upload
    The user needs to upload a file so it can be processed and
    validated. This starts that work by allowing for a simple file upload
    which then echos the user the name of the file they uploaded.

--- app/app.py
+++ app/app.py
@@ -1,11 +1,26 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, request, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+ALLOWED_EXTENSIONS = ['csv']
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
+    if request.method == 'POST':
+        file = request.files['file']
+        filename = None
+        if file and allowed_file(file.filename):
+            filename = file.filename
+            # process file
+
+        return render_template('home.html', upload_filename=filename)
+
     return render_template('home.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
